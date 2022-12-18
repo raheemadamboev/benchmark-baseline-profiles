72adbdf404d0028fd57781a3d42fd32524973ca5
raheemadamboev <vividricemaryandmax@gmail.com>
2022-12-18 15:35:39 +0500

    install dependencies

--- app/src/main/java/xyz/teamgravity/benchmarkbaselineprofile/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/benchmarkbaselineprofile/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.benchmarkbaselineprofile.ui.theme.BenchmarkBaselineProfileTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
